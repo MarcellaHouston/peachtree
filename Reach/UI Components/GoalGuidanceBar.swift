3e99094358519513d4a37fc3846fe2d6c84ba4ae
Rola Owaduge <aderolaowaduge@Rolas-MacBook-Air-2.local>
2026-04-03 16:39:12 -0400

    Created Goal Guidance Popup

--- Reach/UI Components/GoalGuidanceBar.swift
+++ Reach/UI Components/GoalGuidanceBar.swift
@@ -9,16 +9,56 @@ import SwiftUI
 
 struct GoalGuidanceBar: View {
     let prompt: String
-    let clickHandler: () -> Void
+    @State private var isPresenting = false
     
     var body: some View {
         HStack {
-            Circle()
-                .frame(width: 34)
+            Button {
+                isPresenting = true
+            } label: {
+                Circle()
+                    .fill(Color(red: 0.50, green: 0.37, blue: 0.77))
+                    .frame(width: 50, height: 50)
+                    .overlay {
+                        //public library for plus
+                        Image(systemName: "sparkles")
+                            .font(.system(size: 25, weight: .regular))
+                            .foregroundColor(.white)
+                    }
+            }
+            .buttonStyle(.plain)
+            
+                        
             Text(prompt)
         }
-        .onTapGesture {
-            clickHandler()
+        .task {
+            await ApiCall.shared.refreshGoals()
+        }
+        .fullScreenCover(isPresented: $isPresenting,
+                                 onDismiss: didDismiss) {
+            let goal = GoalItemBuilder()
+                .title("Go to the gym 3 times a week.")
+                .category("Fitness")
+                .due(Date(timeIntervalSinceNow: 10000))
+                .mon().wed().fri()
+                .id(4)
+                .build()
+            GuidancePopupView(goal: ApiCall.shared.goals.last ?? goal,
+                              isShowing: $isPresenting)
+            .frame(maxWidth: .infinity,
+                   maxHeight: .infinity)
+            .background(Color.black)
+            .ignoresSafeArea(edges: .all)
         }
     }
+
+
+    func didDismiss() {
+        isPresenting = false
+    }
+    
+}
+
+#Preview {
+        GoalGuidanceBar(prompt: "this is a sample prompt. do you want a suggestion?")
 }
